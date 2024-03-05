f3064a3776a87deb926fdb05a437cd3c75e1aa7f
Стафеев Дмитрий <stafeeff.dmitrij@yandex.ru>
2024-03-05 21:29:11 +0300

    Initial commmit

---
+++ frontend/static/frontend/assets/js/signUp.js
@@ -0,0 +1,18 @@
+var mix = {
+	methods: {
+		signUp () {
+			const name = document.querySelector('#name').value
+			const username = document.querySelector('#login').value
+			const password = document.querySelector('#password').value
+			this.postData('/api/sign-up/', JSON.stringify({ name, username, password }))
+				.then(({ data, status }) => {
+					location.assign(`/`)
+				})
+		}
+	},
+	mounted() {
+	},
+	data() {
+		return {}
+	}
+}
